6424d3f1450b515ee4f27f3b4db303f18cd32d24
A.Paritosh <amulya1171@gmail.com>
2021-08-06 18:03:14 +0530

    ...

--- instabotpy.py
+++ instabotpy.py
@@ -2,43 +2,40 @@ import random
 from instapy import InstaPy
 
 
-
 USERNAME = "indibox.in"
 PASSWORD = "b0@Rd.B1aCK743"
 
-TAGS = []
+TAGS = ["tshirt"]
 COMMENTS = ["Amazing"]
 COMMENT_REPLIES = []
 
 
-
-def getran(a : int, b : int):
+def getran(a: int, b: int):
     return random.randrange(a, b)
-
 
 
 session = InstaPy(username=USERNAME, password=PASSWORD)
 session.login()
 
 session.set_action_delays(
-        enabled= True,
-        like= getran(1,4),
-        comment= getran(2,5),
-        follow= getran(3,6),
-        randomize= True,
-        random_range_from= getran(1,3),
-        random_range_to= getran(3,6)
-        )
+    enabled=True,
+    like=getran(1, 2),
+    comment=getran(1, 3),
+    follow=getran(1, 4),
+    randomize=True,
+    random_range_from=getran(1, 2),
+    random_range_to=getran(3, 4)
+)
 
-session.like_by_tags(TAGS, amount= getran(getran(5,11), getran(11,21)))
-session.set_dont_like(["naked", "nsfw"])
+session.like_by_tags(TAGS, amount=getran(getran(5, 11), getran(11, 21)))
+# session.set_dont_like(["naked", "nsfw"])
 
-session.set_do_follow(True, percentage= getran(50,100))
+session.set_do_follow(True, percentage=getran(50, 100))
 
-session.set_comments(comments= COMMENTS)
-session.set_do_comment(True, percentage= getran(50,80))
+# session.set_comments(comments=COMMENTS)
+# session.set_do_comment(True, percentage=getran(50, 80))
 
-session.set_comment_replies(replies= COMMENT_REPLIES )
-session.set_do_reply_to_comments(True, percentage= getran(90,101))
+# session.set_comment_replies(replies=COMMENT_REPLIES)
+# session.set_do_reply_to_comments(True, percentage=getran(90, 101))
 
 session.end()
